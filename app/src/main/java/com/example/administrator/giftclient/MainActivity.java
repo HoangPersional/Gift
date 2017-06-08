2d3155d8913ec244c7d42b69405b99959408d9e4
nguyenngochoang080190 <nguyenngochoang080190@gmail.com>
2017-06-09 00:02:48 +0700

    z

--- app/src/main/java/com/example/administrator/giftclient/MainActivity.java
+++ app/src/main/java/com/example/administrator/giftclient/MainActivity.java
@@ -9,19 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-View m;
-View j;
-View k;
-View z;
-View ii;
-View iiii;
-View mm;
-View uouoi;
-
-        View anhyeuem;
- View anhyeuem1;
-
-        View sogoku;
-
     }
 }
