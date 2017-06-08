8f590ef61715ff8cb360493ea6f63bc9c59ab812
nguyenngochoang080190 <nguyenngochoang080190@gmail.com>
2017-06-08 23:10:22 +0700

    h

--- app/src/main/java/com/example/administrator/giftclient/MainActivity.java
+++ app/src/main/java/com/example/administrator/giftclient/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 View m;
-View kk;
+View kkk;
     }
 }
