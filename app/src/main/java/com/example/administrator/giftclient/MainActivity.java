71c79e17781caff1503ae9f6e17016a6cbe29f79
nguyenngochoang0801 <maimanhoveban@gmail.com>
2017-06-09 06:36:18 +0700

    ma

--- app/src/main/java/com/example/administrator/giftclient/MainActivity.java
+++ app/src/main/java/com/example/administrator/giftclient/MainActivity.java
@@ -8,16 +8,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-setContentView(R.layout.activity_main);
-
-
-View k;
-
-setContentView(R.layout.activity_main);
-        View z;
-View t;
-View m;
-View h;
     }
 }
